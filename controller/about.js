f146d7b909f5ffc74520d47ec2beadc0e30d41f2
Santosh <sk081jha@gmail.com>
2025-05-07 19:42:13 +0530

    startint next day

--- controller/about.js
+++ controller/about.js
@@ -1,7 +1,9 @@
 const aboutHandler = async (req,res)=>{
    
     const finalData = {result:[{name:'website'},{author:'santosh jha'},{role:'developer'},{purpose:'practice'},{route:'about section'}]}
-
+    res.setHeader("myName","Santosh")
+    console.log(req.headers);
+    
     return res.status(200).json(finalData)
 }
 
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require ("express");
 const apiRoutes = require('./routes/apis')
 const port = 3000
 
-const app = express();
-
+const app = express()
 app.use(express.json());
 
 app.use(express.urlencoded({extended:true}))
